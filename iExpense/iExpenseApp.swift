3e254677972a537e69b6b25f5cafa0be257be585
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-03-18 15:24:47 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Igor Florentino on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
