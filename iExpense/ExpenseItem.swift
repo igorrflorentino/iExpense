5de163613dda22ed9227e282f3476b4557aa0fc7
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-05-24 11:13:59 -0300

    cleaning model

--- iExpense/ExpenseItem.swift
+++ iExpense/ExpenseItem.swift
@@ -7,38 +7,11 @@
 
 import Foundation
 
-struct ExpenseItem: Identifiable, Codable, Equatable{
+struct ExpenseItem{
 	var id = UUID()
 	let name: String
 	let type: String
 	let amount: Double
 }
 
-@Observable
-class Expenses {
-	var allItens = [ExpenseItem]() {
-		didSet{
-			if let itensJSON = try? JSONEncoder().encode(allItens){
-				UserDefaults.standard.set(itensJSON, forKey: "itens")
-			}
-		}
-	}
-	
-	var personalItens: [ExpenseItem] {
-		allItens.filter{ $0.type == "Personal"}
-	}
-	var businessItens: [ExpenseItem] {
-		allItens.filter{ $0.type == "Business"}
-	}
-	
-	init(){
-		if let itensJSON = UserDefaults.standard.data(forKey: "itens"){
-			if let decodedItens = try? JSONDecoder().decode([ExpenseItem].self, from: itensJSON){
-				allItens = decodedItens
-				return
-			}
-		}
-		allItens = []
-	}
-}
 
